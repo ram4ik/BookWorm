79599660b603584428fbd2595f03cd04eec76388
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-12-10 17:15:24 +0200

    Deleting from a Core Data fetch request

--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -33,9 +33,10 @@ struct ContentView: View {
                         }
                     }
                 }
+            .onDelete(perform: deleteBooks)
             }
-                .navigationBarTitle("Bookworm")
-                .navigationBarItems(trailing:
+            .navigationBarTitle("Bookworm")
+            .navigationBarItems(leading: EditButton(), trailing:
                     Button(action: {
                         self.showingAddScreen.toggle()
                     }) {
@@ -46,6 +47,15 @@ struct ContentView: View {
                     AddBookView().environment(\.managedObjectContext, self.moc)
             }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        
+        try? moc.save()
     }
 }
 
